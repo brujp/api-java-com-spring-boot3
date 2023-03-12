508a2b74c706e0785fe9cfd2901eb8c894e8c0a0
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2023-03-12 11:32:24 -0300

    adding notes to the class

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -3,5 +3,6 @@ package med.voll.api.medico;
 import med.voll.api.endereco.DadosEndereco;
 
 //Classe do tipo Record - Novo recurso do Java
+//https://docs.oracle.com/en/java/javase/16/language/records.html
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
